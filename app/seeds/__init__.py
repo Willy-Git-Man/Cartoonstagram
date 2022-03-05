7844f2988bd294dbb250f20f214b77d72ad765d7
Brendon <brendonwjames@gmail.com>
2022-03-04 19:28:46 -0800

    added seeds for comments

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .posts import seed_posts, undo_posts
+from .comments import seed_comment, undo_comment
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -13,6 +14,7 @@ def seed():
     seed_users()
     # Add other seed functions here
     seed_posts()
+    seed_comment()
 
 
 # Creates the `flask seed undo` command
@@ -21,4 +23,4 @@ def undo():
     undo_users()
     # Add other undo functions here
     undo_posts()
-    
+    undo_comment()
--- app/seeds/posts.py
+++ app/seeds/posts.py
@@ -3,15 +3,14 @@ from app.models.post import db, Post
 # Adds a demo user, you can add other users here if you want
 def seed_posts():
     demo = Post(
-        id=1, user_id=1, img_src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6hG9apgw_VGzfBWwC7cmm2ALp60d0e-G_sA&usqp=CAU", caption_content='1 2 3 4', location='Aron City')
-    # marnie = User(
-    #     username='marnie', email='marnie@aa.io', password='password')
-    # bobbie = User(
-    #     username='bobbie', email='bobbie@aa.io', password='password')
-
+        id=1, 
+        user_id=1, 
+        img_src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6hG9apgw_VGzfBWwC7cmm2ALp60d0e-G_sA&usqp=CAU", 
+        caption_content='1 2 3 4',
+        location='Aron City'
+    )
+    
     db.session.add(demo)
-    # db.session.add(marnie)
-    # db.session.add(bobbie)
 
     db.session.commit()
 
