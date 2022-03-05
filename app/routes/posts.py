929aecadf01e1a59ea2d4cc50522ca4d6cd42047
Leonel Murillo <ljmurill@uci.edu>
2022-03-05 11:59:37 -0800

    POST for post

--- app/routes/posts.py
+++ app/routes/posts.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify
+from datetime import datetime
+from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
 
 from app.models.db import db
@@ -8,24 +9,26 @@ from app.forms.post_form import PostForm
 posts = Blueprint('posts', __name__)
 
 
-@posts.route('/')
+@posts.route('')
 def get_post():
     allpost = Post.query.all()
     print(allpost)
     return {'posts': [post.to_dict() for post in allpost]}
 
-@posts.route('/', methods=["POST"])
+@posts.route('', methods=["POST"])
 def create_post():
     form = PostForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
+        print('Helloee')
         post = Post(
-            user_id=current_user.id,
+            user_id=form.data['user_id'],
             img_src=form.data['img_src'],
             caption_content=form.data['caption_content'],
             location=form.data['location'],
-            created_at=form.data['created_at']
+            created_at=datetime.now()
         )
         db.session.add(post)
         db.session.commit()
         return post.to_dict()
-    
+   
--- react-app/src/store/posts.js
+++ react-app/src/store/posts.js
@@ -15,23 +15,46 @@ const createPost = (post) => ({
 
 
 export const allPost = () => async(dispatch) => {
-    const response = await fetch('/api/posts');
+    const response = await fetch('/posts');
 
     if(response.ok) {
         const posts = await response.json();
         dispatch(getPost(posts))
     }
+    return response;
 
 }
 
-const initialState = {posts: {}}
+export const makePost = (post) => async(dispatch) => {
+    console.log('JHEll', post)
+    const response = await fetch('/posts', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(post)
+    })
+
+    if(response.ok){
+        const newPost = await response.json();
+        dispatch(createPost(newPost))
+    }
+    return response;
+}
+
+const initialState = {posts: []}
 
 export default function postReducer(state = initialState, action) {
+    let newState;
     switch (action.type){
-        let newState;
         case GET_POSTS:
             newState = {...state}
+            action.allPost.forEach(post => newState[post.id] = post)
             newState.posts = [...action.allPost]
             return newState
+        case CREATE_POST:
+            newState={...state}
+            newState.posts = [...newState.posts, action.newPost];
+            return newState
+        default:
+            return state
     }
 }
