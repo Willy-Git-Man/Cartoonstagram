24811ff89df5ab2a08884124d6e8d8ff1bdc038f
Willy-Git-Man <83353737+Willy-Git-Man@users.noreply.github.com>
2022-03-08 18:28:52 -0500

    likes finished

--- react-app/src/components/Modals/PostsForm/SpecificPost/MainPageModal.js
+++ react-app/src/components/Modals/PostsForm/SpecificPost/MainPageModal.js
@@ -1,65 +1,53 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import {allLike, deleteLike, makeLike} from '../../../../store/likes'
-
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { allLike, deleteLike, makeLike } from "../../../../store/likes";
 
 const MainPageModal = ({ modalInfo }) => {
   const dispatch = useDispatch();
-  const currentUser = useSelector(state => state.session.user);
-  const currentUserLiked = useSelector(state => state.likes);
+  const currentUser = useSelector((state) => state.session.user);
+  const currentUserLiked = useSelector((state) => state.likes.likes);
 
-  const likeValuesArr = Object.values(currentUserLiked)
-  console.log(likeValuesArr, 'please be here')
-
-
-
-  console.log(likeIncludes, "==========")
-
-  console.log('here we are', currentUserLiked)
-
+  console.log("currentUserLiked:", currentUserLiked);
   useEffect(() => {
-    dispatch(allLike(modalInfo.id))
-  }, [ modalInfo.id, dispatch ])
+    dispatch(allLike(modalInfo.id));
+  }, [modalInfo.id, dispatch]);
 
-  const handleLike= async() => {
-    console.log('from handleLike in MainPageModal')
-    console.log(modalInfo.id)
-    dispatch(makeLike(modalInfo.id))
-  }
+  const handleLike = async () => {
+    console.log("from handleLike in MainPageModal");
+    console.log(modalInfo.id);
+    dispatch(makeLike(modalInfo.id));
+  };
 
-  const handleDeleteLike= async() => {
-    dispatch(deleteLike(modalInfo.id))
-  }
-
-      let likeIncludes = likeValuesArr.forEach(likeObj => {
-    console.log(likeObj, 'ppppppppp')
-    console.log(likeObj.user_id, currentUser.id)
-    if (likeObj.user_id === currentUser.id) {
-      return true
-    }
-    return false
-  })
-  if (likeIncludes)
-
-  return (
-    <div>
+  const handleDeleteLike = async () => {
+    dispatch(deleteLike(modalInfo.id));
+  };
+  // if ((currentUserLiked[0].user_id === currentUser.id) === true)
+  // if (likeValuesArr[0]/)
+  if (currentUserLiked.length > 0)
+    return (
       <div>
-        <img src={modalInfo.img_src} />
+        <div>
+          <img src={modalInfo.img_src} alt="Falty Url"/>
+        </div>
+        <div>{modalInfo.caption_content}</div>
+
+        {/* <button onClick={handleLike}>Like</button> */}
+        <button onClick={handleDeleteLike}>Unlike</button>
       </div>
+    );
+  else
+    return (
       <div>
-        {modalInfo.caption_content}
+        <div>
+          <img src={modalInfo.img_src} alt="Falty Url"/>
+        </div>
+        <div>{modalInfo.caption_content}</div>
+
+        <button onClick={handleLike}>Like</button>
+        {/* <button onClick={handleDeleteLike}>Unlike</button> */}
       </div>
-
-     
-      <button onClick={handleLike}>Like</button>
-      <button onClick={handleDeleteLike}>Unlike</button>
-
-    </div>
-  );
-  if (!likeIncludes) return 'trying something'
-  
-}
+    );
+};
 
 export default MainPageModal;
--- react-app/src/store/likes.js
+++ react-app/src/store/likes.js
@@ -1,87 +1,90 @@
-const GET_LIKES = 'likes/GET_LIKES';
-const CREATE_LIKE = 'likes/CREATE_LIKE';
-const REMOVE_LIKE = 'likes/REMOVE_LIKE'
+const GET_LIKES = "likes/GET_LIKES";
+const CREATE_LIKE = "likes/CREATE_LIKE";
+const REMOVE_LIKE = "likes/REMOVE_LIKE";
 
 const getLike = (allLike) => ({
-    type: GET_LIKES,
-    allLike: allLike
-})
-
+  type: GET_LIKES,
+  allLike,
+});
 
 const createLike = (newLike) => ({
-    type: CREATE_LIKE,
-    newLike
-})
+  type: CREATE_LIKE,
+  newLike,
+});
 
 const removeLike = (oldLike) => ({
-    type: CREATE_LIKE,
-    oldLike
-})
+  type: REMOVE_LIKE,
+  oldLike,
+});
 
+export const allLike = (id) => async (dispatch) => {
+  const response = await fetch(`/likes/${id}`);
 
+  if (response.ok) {
+    const posts = await response.json();
+    dispatch(getLike(posts));
+  }
+  return response;
+};
 
-export const allLike = (id) => async(dispatch) => {
-    const response = await fetch(`/likes/${id}`);
+export const makeLike = (like) => async (dispatch) => {
+  const response = await fetch(`/likes/${like}`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(like),
+  });
 
-    if(response.ok) {
-        const posts = await response.json();
-        dispatch(getLike(posts))
-    }
-    return response;
-
-}
-
-export const makeLike = (like) => async(dispatch) => {
-    const response = await fetch(`/likes/${like}`, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(like)
-    })
-
-    if(response.ok){
-        const newLike = await response.json();
-        dispatch(createLike(newLike))
-    }
-    return response;
-}
+  if (response.ok) {
+    const newLike = await response.json();
+    dispatch(createLike(newLike));
+  }
+  return response;
+};
 
 export const deleteLike = (like) => async (dispatch) => {
-    const response = await fetch(`likes/${like}`, {
-      method: "DELETE",
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    });
+  const response = await fetch(`likes/${like}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    if (response.ok) {
-      dispatch(removeLike());
-    }
-  };
 
-const initialState = {likes: []}
+  if (response.ok) {
+      const like = await response.json()
+    dispatch(removeLike(like));
+  }
+};
+
+const initialState = { likes: [] };
 
 export default function likeReducer(state = initialState, action) {
-    let newState;
-    switch (action.type){
-        case GET_LIKES:
-            console.log(action, 'what is the action')
-            newState = {...state}
-            newState.likes.forEach(post => delete newState[post.id] )
+  let newState;
+  switch (action.type) {
+    case GET_LIKES:
+      console.log(action, "what is the action");
+      newState = { ...state };
+      newState.likes.forEach((like) => delete newState[like.id]);
+      newState.likes = [...action.allLike.likes];
+      newState.likes.forEach((like) => (newState[like.id] = like));
+      return newState;
 
-            newState.likes = [...action.allLike.likes]
-            newState.likes.forEach(post => newState[post.id] = post)
-            return newState
-        case CREATE_LIKE:
-            newState={...state}
-            newState.likes = [...newState.likes, action.newPost];
-            return newState
 
-        case REMOVE_LIKE:
-            newState = {...state}
-            delete newState[action.like.id]
-            return newState
+    case CREATE_LIKE:
+      newState = { ...state };
+      newState.likes.forEach((like) => delete newState[like.id]);
+      newState.likes = [...newState.likes, action.newLike];
+      newState.likes.forEach((like) => (newState[like.id] = like));
+      return newState;
 
-        default:
-            return state
-    }
+    case REMOVE_LIKE:
+      newState = { ...state };
+      delete newState[action.oldLike.id];
+      newState.likes.splice(newState.likes.indexOf(action.oldLike), 1);
+      newState.likes = [...newState.likes]
+      return newState;
+
+    default:
+      return state;
+  }
 }
