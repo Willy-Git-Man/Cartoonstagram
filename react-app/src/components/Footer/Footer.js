73ddd394f88fd8a1389a65097a779f5964cfef6e
Brendon <brendonwjames@gmail.com>
2022-03-10 13:16:16 -0800

    added footer

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -2,17 +2,22 @@ import React from 'react';
 
 import './Footer.css';
 
-const brendon = <a href="https://github.com/brendonwjames" target='_blank'>Brendon James</a>
-const jacob = <a href="https://github.com/JacobHoldowsky" target='_blank'>Jacob Holdowsky</a>
-const leonel = <a href="https://github.com/ljmurill" target='_blank'>Leonel Murillo</a>
-const will = <a href="https://github.com/Willy-Git-Man" target='_blank'>Will Grossman</a>
+const brendon = <a href="https://github.com/brendonwjames" target='_blank'><i class="fa-solid fa-user"></i>Brendon James</a>
+const jacob = <a href="https://github.com/JacobHoldowsky" target='_blank'><i class="fa-solid fa-user"></i>Jacob Holdowsky</a>
+const leonel = <a href="https://github.com/ljmurill" target='_blank'><i class="fa-solid fa-user"></i>Leonel Murillo</a>
+const will = <a href="https://github.com/Willy-Git-Man" target='_blank'><i class="fa-solid fa-user"></i>Will Grossman</a>
 
 const Footer = () => {
   return (
     <div className='footer'>
       <div className='footer-content'>
-        <div className='footer-header'>Created by {brendon}, {jacob}, {leonel}, and {will}</div>
+        <div className='footer-header'>Created by {brendon} {jacob} {leonel} {will}</div>
+          {/* <a href='https://www.linkedin.com/' target='_blank' rel=''>
+            <i className="fab fa-linkedin"></i>
+            LinkedIn links?
+          </a> */}
           <a href='https://github.com/Willy-Git-Man/Cartoonstagram' target='_blank' rel='noopener noreferrer'>
+            <i class="fa-solid fa-user"></i>
             Project GitHub
           </a>
         </div>
