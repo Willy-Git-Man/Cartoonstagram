f25475a9848afc160d75708e69e8ae4d8ce974eb
Willy-Git-Man <83353737+Willy-Git-Man@users.noreply.github.com>
2022-03-11 14:03:44 -0500

    working on follow stuff

--- react-app/src/components/UserList/UsersList.js
+++ react-app/src/components/UserList/UsersList.js
@@ -1,35 +1,75 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
-import './userList.css'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { NavLink, useParams } from "react-router-dom";
+import "./userList.css";
 
 function UsersList() {
+
+
+  const [user, setUser] = useState({});
+  const [userPosts, setUserPosts] = useState([]);
+  const [followeds, setFolloweds] = useState([]);
+  const [followers, setFollowers] = useState([]);
+  const { userId }  = useParams();
+
+
+
   const [users, setUsers] = useState([]);
-  const user = useSelector((state) => state.session.user)
+  // const user = useSelector((state) => state.session.user);
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch('/api/users/');
+      const response = await fetch("/api/users/");
       const responseData = await response.json();
       setUsers(responseData.users);
     }
     fetchData();
   }, []);
 
+
+
+  useEffect(() => {
+    dispatch(userFollowers(parseInt(currentUser.id)))
+
+    if (!userId) {
+      return;
+    }
+
+    (async () => {
+      const response = await fetch(`/api/users/${userId}`);
+      const user = await response.json();
+      setUser(user.user);
+      setUserPosts(user.posts)
+
+    })();
+
+    (async () => {
+      const response = await fetch(`/follows/${userId}/followeds`)
+      const followeds = await response.json()
+      setFolloweds(followeds.follows);
+    })();
+
+    (async () => {
+      const response = await fetch(`/follows/${userId}/followers`)
+      const followers = await response.json()
+      setFollowers(followers.followers.length);
+    })();
+
+  }, [userId, dispatch, currentUser.id]);
+
   const userComponents = users.map((user) => {
     return (
       <div className="followerDivEach">
         <img className="pictureImg" src={user.img_src} alt="Broken Img URL" />
-        <NavLink className="followNavLink" to={`/users/${user.id}`}>{user.username}</NavLink>
-
+        <NavLink className="followNavLink" to={`/users/${user.id}`}>
+          {user.username}
+        </NavLink>
       </div>
-
-
     );
   });
 
   return (
     <>
-    {/* <div className="followerUserNav">
+      {/* <div className="followerUserNav">
     <img className="picImgUserFollower" src={user.profile_img_src} alt="Broken Img URL"/>
     <h1>{user.username}</h1>
   <NavLink to={`/users/${user.id}`}>{user.username}</NavLink>
